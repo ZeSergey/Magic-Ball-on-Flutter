7b3d22e062ca340e6b5afd0d5c59103f5e355566
MacBook <macbook@MacBook-Pro-MacBook.local>
2020-05-18 15:28:52 +0300

    add State

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
+import 'Dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MagicBall(),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  @override
+  int ball = Random().nextInt(5) + 1;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Ask Me Anything",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+        ),
+        backgroundColor: Colors.blue[800],
+      ),
+      body: Container(
+        color: Colors.lightBlue,
+        child: Center(
+          child: FlatButton(
+              child: Image.asset('images/ball$ball.png'),
+              onPressed: () {
+                setState(() {
+                  ball = Random().nextInt(5) + 1;
+                });
+              }),
+        ),
+      ),
+    );
+  }
+}
